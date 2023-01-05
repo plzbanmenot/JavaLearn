0bf4221f0e6fe0d57db9ff8ac826a0894689afeb
plzbanmenot <plzbanmenot@hotmail.com>
2023-01-06 02:05:18 +0500

    DateTimeCommit2

--- src/src/main/java/org/example/lambda/DoubleTwoPointsOpTest.java
+++ src/src/main/java/org/example/lambda/DoubleTwoPointsOpTest.java
@@ -5,5 +5,19 @@ import java.util.List;
 public class DoubleTwoPointsOpTest {
     public  static void main(String args[]){
         List.of(1,2,3,5,6).stream().map(Integer::toBinaryString).forEach(System.out::println);
+        IntMultipleTestInterface intMultipleTestInterface =  x-> MultiTest.multiple(x);
+        List.of(1,2,3,5,6).stream().map(MultiTest::multiple).forEach(System.out::println);
+        List.of(1,2,3,5,6).stream().map(x ->MultiTest.test(intMultipleTestInterface, x)).forEach(System.out::println);
+        //List.of(1,2,3,5,6).stream().map().forEach(System.out::println);
+    }
+}
+
+class MultiTest{
+    static int multiple(int a){
+        return a * a;
+    }
+
+    static int test(IntMultipleTestInterface i, int x){
+        return i.multiply(x);
     }
 }
---
+++ src/src/main/java/org/example/lambda/IntMultipleTestInterface.java
@@ -0,0 +1,5 @@
+package org.example.lambda;
+
+public interface IntMultipleTestInterface {
+    int multiply(int a);
+}
